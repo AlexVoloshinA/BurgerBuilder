5be0cf5d9bbc4cd9d744473e622551fc791d20ca
Alex Voloshin <avoloshin225@gmail.com>
2018-09-21 17:39:33 +0300

    Adding adaptive

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Aux from "../../hoc/Auxxxxx";
 import classes from "./Layout.css";
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from "../../components/Layout/Layout";
 
 const layout = props => (
   <Aux>
-    <div>Toolbar, SideDrawer, BackDrop</div>
+    <Toolbar/>
+    {/* <SideDrawer/> */}
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
